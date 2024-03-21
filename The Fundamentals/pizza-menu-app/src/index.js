bfdd41b3b74e1f4fa60d73a6ac18e1d3064c12bb
Irakli <112602982+IkaMastera@users.noreply.github.com>
2024-03-21 12:43:08 +0400

    learned: about conditional rendering
    using && operator or ternaries or multiple return statements

--- The Fundamentals/pizza-menu-app/src/index.js
+++ The Fundamentals/pizza-menu-app/src/index.js
@@ -66,15 +66,22 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
+
   return (
     <main className="menu">
       <h2>Our menu</h2>
 
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {numPizzas > 0 ? (
+        <ul className="pizzas">
+          {pizzas.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      ) : (
+        <p>No Pizzas Left</p>
+      )}
     </main>
   );
 }
@@ -93,7 +100,7 @@ function Pizza(props) {
 }
 
 function Footer() {
-  const hour = new Date().getHours;
+  const hour = new Date().getHours();
   const openHour = 12;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
@@ -101,7 +108,14 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString} We're currently open
+      {isOpen ? (
+        <div className="order">
+          <p>We're open until {closeHour}:00. Come visit us or order online</p>
+          <button className="btn">Order</button>
+        </div>
+      ) : (
+        <p>We're Closed</p>
+      )}
     </footer>
   );
 }
